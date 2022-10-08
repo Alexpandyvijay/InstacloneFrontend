4c30f9fadc5dac044773db161a643705aa6ce833
alexpandy <alexpandyvijay@gmail.com>
2022-10-08 17:43:00 +0530

    frist release of user Interface

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import './App1.css';
+import Postview from './PostView/postview';
+import LandingPage from './LandingPage/landingpage';
+import PostUpload from './upload/uploadpost';
+
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+            <Route path='/' element={<LandingPage/>}/>
+            <Route path='/post' element={<Postview/>}/>
+            <Route path='/upload' element={<PostUpload/>}/>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
